50fa1142e103c1b99d548974966b6638c7bdc4db
Kristiyan Tachev <kristiqn.tachev@gmail.com>
2018-07-17 02:25:46 +0300

    1.0.20

--- src/app/core/api-introspection/EffectTypes.ts
+++ src/app/core/api-introspection/EffectTypes.ts
@@ -1,10 +1,10 @@
 
+/* tslint:disable */
 function strEnum<T extends string>(o: Array<T>): {[K in T]: K} {
     return o.reduce((res, key) => {
         res[key] = key;
         return res;
     }, Object.create(null));
 }
-export const EffectTypes = strEnum(['findUser',
-'findUser2']);
+export const EffectTypes = strEnum(["findApp"]);
 export type EffectTypes = keyof typeof EffectTypes;
